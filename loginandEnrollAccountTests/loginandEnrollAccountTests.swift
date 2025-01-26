a68a455db753dd86a1e73278f271ec2e9d1a9611
Raheem <rchisman@microsoft.com>
2025-01-26 05:05:54 -0800

    Initial Commit

---
+++ loginandEnrollAccountTests/loginandEnrollAccountTests.swift
@@ -0,0 +1,17 @@
+//
+//  loginandEnrollAccountTests.swift
+//  loginandEnrollAccountTests
+//
+//  Created by Raheem Chisman on 1/26/25.
+//
+
+import Testing
+@testable import loginandEnrollAccount
+
+struct loginandEnrollAccountTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
